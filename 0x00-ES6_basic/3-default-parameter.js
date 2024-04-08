5897d344bf397915927353526548a2221a9d2120
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-08 15:25:23 +0000

    3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,5 +1,4 @@
 export default function getSumOfHoods(initialNumber = 34, expansion1989 = 89, expansion2019 = 19) {
-  /* eslint-disable no-param-reassign */
   if (expansion1989 === undefined) {
     expansion1989 = 89;
   }
@@ -8,5 +7,4 @@ export default function getSumOfHoods(initialNumber = 34, expansion1989 = 89, ex
     expansion2019 = 19;
   }
   return initialNumber + expansion1989 + expansion2019;
-  /* eslint-disable no-param-reassign */
 }
