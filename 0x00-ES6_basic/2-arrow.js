63f35281aa919d48a1cfbdcc814ea019f3c39271
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-08 12:59:23 +0000

    2-arrow.js

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList() {
+  const sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const addNeighborhood = (newNeighborhood) => {
+    sanFranciscoNeighborhoods.push(newNeighborhood);
+    return sanFranciscoNeighborhoods;
+  };
+  return { sanFranciscoNeighborhoods, addNeighborhood };
+}
