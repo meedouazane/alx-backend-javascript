f8fc37ab6b72c7d25baafdce14d5e50407720e8b
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-11 20:30:28 +0000

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,32 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a String');
+    }
+    this._code = code;
+  }
+
+  toString() {
+    return `[${this._code}]`;
+  }
+}
