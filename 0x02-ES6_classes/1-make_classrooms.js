9d0f8dd22f413c774a5e9da43e2aa39a85eb39c4
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-11 14:21:00 +0000

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,8 +2,8 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
-    new Classroom(19),
-    new Classroom(20),
-    new Classroom(34),
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
   ];
 }
