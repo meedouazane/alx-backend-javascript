d11c6c191bc708c5787c849e0e2afcfd3d2acb3f
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-11 22:08:00 +0000

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,38 +1,37 @@
 export class HolbertonClass {
-    constructor(year, location) {
-        this._year = year;
-        this._location = location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
 
-    get year() {
-        return this._year;
-    }
+  get year() {
+    return this._year;
+  }
 
-    get location() {
-        return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 }
 
 export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._holbertonClass = holbertonClass;
-    }
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
 
-    get fullName() {
-        return `${this._firstName} ${this._lastName}`;
-    }
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 
-    get holbertonClass() {
-        return this._holbertonClass;
-    }
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
 
-    get fullStudentDescription() {
-        return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -41,10 +40,5 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
 const listOfStudents = [student1, student2, student3, student4, student5];
-
-const listPrinted = listOfStudents.map(student => student.fullStudentDescription);
-
-console.log(listPrinted);
-
+export default listOfStudents;
