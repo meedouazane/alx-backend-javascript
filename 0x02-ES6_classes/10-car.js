0ea2a52a0bc3e426c1a2ccb314d1ec4e1865efb7
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-11 15:28:07 +0000

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(brand) {
+    this._brand = brand;
+  }
+
+  set motor(motor) {
+    this._motor = motor;
+  }
+
+  set color(color) {
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
