8eeb6d29e104fdeed2cec27716c3664b3a3565fa
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-11 15:40:36 +0000

    100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,26 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
