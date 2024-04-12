9ea7212c1187a9002637cd228a0977a42990226b
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-12 13:35:17 +0000

    8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,10 +1,10 @@
 export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
   try {
-    if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    }
-    const result = numerator / denominator;
-    return result;
-  } finally {
+    return numerator / denominator;
+  } catch (e) {
+    return (e);
   }
 }
