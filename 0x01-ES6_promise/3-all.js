14fbbcaa6f7c2a6e0d5ad9d53506ff23b7b67d7d
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-12 14:40:21 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,11 @@
-import uploadPhoto from "./utils.js";
-import createUser from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	return new Promise(function(resolve, reject) {
-		uploadPhoto.then(response => {
-			if (response === 
+  const promises = [createUser(), uploadPhoto()];
+  Promise.all(promises).then((results) => {
+    const [userResult, photoResult] = results;
+    const Name = `${userResult.firstName} ${userResult.lastName}`;
+    const { body } = photoResult;
+    console.log(`${body} ${Name}`);
+  }).catch((e) => console.log(e));
 }
