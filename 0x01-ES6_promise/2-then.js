d8062e06ccc044c442ad15558c7884c5af379467
mohamed ouazane <mohamedouazane@yahoo.com>
2024-04-12 15:31:44 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise(((resolve, reject) => {
-    promise.then((response) => {
-      if (response === true) {
-        resolve({ status: 200, body: 'Success' });
-      }
-    }).catch(() => {
-      reject(new Error());
-    }).finally(() => {
-      console.log('Got a response from the API');
-    });
-  }));
+  promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => (new Error()))
+    .finally(() => { console.log('Got a response from the API'); });
 }
