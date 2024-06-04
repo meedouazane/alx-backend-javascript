67b44ccbf974b0669a549e4fb5ba9e63c1989bad
MO <mohamedouazane@yahoo.com>
2024-06-04 12:10:37 +0000

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,27 +3,37 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.toString().split('\n');
-    const validLine = lines.filter((line) => line.trim() !== '');
+    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+    if (lines.length <= 1) {
+      throw new Error('No students found in the database');
+    }
+
     let CS = 0;
     const csList = [];
     let SWE = 0;
     const sweList = [];
+
     for (let i = 1; i < lines.length; i += 1) {
-      const [firstname, , , field] = lines[i].split(',');
-      if (field === 'CS') {
-        CS += 1;
-        csList.push(firstname);
-      } else if (field === 'SWE') {
-        SWE += 1;
-        sweList.push(firstname);
+      const line = lines[i].split(',');
+      if (line.length >= 4) {
+        const firstname = line[0].trim();
+        const field = line[3].trim();
+        if (field === 'CS') {
+          CS += 1;
+          csList.push(firstname);
+        } else if (field === 'SWE') {
+          SWE += 1;
+          sweList.push(firstname);
+        }
       }
     }
-    console.log('Number of students:', validLine.length - 1);
+
+    console.log('Number of students:', lines.length - 1);
     console.log(`Number of students in CS: ${CS}. List: ${csList.join(', ')}`);
     console.log(`Number of students in SWE: ${SWE}. List: ${sweList.join(', ')}`);
   } catch (err) {
-    throw new Error('Cannot load the database');
+    console.error('Cannot load the database:', err.message);
   }
 }
+
 module.exports = countStudents;
