9e6efb0e5e0564852414b4a59491d8041b3a3047
MO <mohamedouazane@yahoo.com>
2024-06-04 11:30:55 +0000

    2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.split('\n');
+    const validLine = lines.filter((line) => line.trim() !== '');
+    let CS = 0;
+    const csList = [];
+    let SWE = 0;
+    const sweList = [];
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstname, , , field] = lines[i].split(',');
+      if (field === 'CS') {
+        CS += 1;
+        csList.push(firstname);
+      } else if (field === 'SWE') {
+        SWE += 1;
+        sweList.push(firstname);
+      }
+    }
+    console.log('Number of students:', validLine.length - 1);
+    console.log(`Number of students in CS: ${CS}. List: ${csList.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWE}. List: ${sweList.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
