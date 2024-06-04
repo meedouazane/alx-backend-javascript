78514baf1496c93fff36e0bbc6ee4d26d9e840ae
MO <mohamedouazane@yahoo.com>
2024-06-04 13:20:52 +0000

    500

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -20,7 +20,7 @@ class StudentsController {
     const field = request.params.major;
     readDatabase(process.argv[2].toString()).then((students) => {
       if (!(field in students)) {
-        response.status(200).send('Major parameter must be CS or SWE');
+        response.status(500).send('Major parameter must be CS or SWE');
       } else {
         response.status(200).send(`List: ${students[field].join(', ')}`);
       }
