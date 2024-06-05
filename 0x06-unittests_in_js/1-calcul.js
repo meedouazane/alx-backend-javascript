2dbdcbec30c7e2146eaee2ba8d9ea4b2142b98b8
MO <mohamedouazane@yahoo.com>
2024-06-05 19:17:21 +0000

    1-calcul.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,13 +1,12 @@
 function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-  } if (type === 'SUBTRACT') {
+  }
+  if (type === 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
-  } if (type === 'DIVIDE') {
-    if (b === 0) {
-      return 'Error';
-    }
-    return Math.round(a) / Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
   return 0;
 }
