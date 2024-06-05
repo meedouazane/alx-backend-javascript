94d33d61dc39ab9c2f30ebd30192a6d63300d8a2
MO <mohamedouazane@yahoo.com>
2024-06-05 19:26:55 +0000

    utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,16 +1,18 @@
 const Utils = {
-	calculateNumber(type, a, b) {
+  calculateNumber(type, a, b) {
     if (type === 'SUM') {
-        return Math.round(a) + Math.round(b)
-    } else  if (type === 'SUBTRACT') {
-        return Math.round(a) - Math.round(b)
-    } else  if (type === 'DIVIDE') {
-        if (b === 0) {
-            return 'Error'
-        } else {
-            return Math.round(a) / Math.round(b)
-        }
+      return Math.round(a) + Math.round(b);
     }
-}
-}
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    return 0;
+  },
+};
 module.exports = Utils;
