c15dce86e953222136e90f5c03494a10a7b28b0d
MO <mohamedouazane@yahoo.com>
2024-06-05 19:22:11 +0000

    2-calcul_chai.

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,13 +1,16 @@
 function calculateNumber(type, a, b) {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-  } if (type === 'SUBTRACT') {
+  }
+  if (type === 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
-  } if (type === 'DIVIDE') {
-    if (b === 0) {
+  }
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
       return 'Error';
     }
     return Math.round(a) / Math.round(b);
   }
-}
+  return 0;
+};
 module.exports = calculateNumber;
