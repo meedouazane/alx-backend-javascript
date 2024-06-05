e2f98590da409829063e062dba5aebad67f7b970
MO <mohamedouazane@yahoo.com>
2024-06-05 21:11:00 +0000

    6-payment_token.

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+    it('getPaymentTokenFromAPI(success), where success == true', (done) => {
+        getPaymentTokenFromAPI(true)
+          .then((res) => {
+            expect(res).to.deep.equal({ data: 'Successful response from the API' });
+            done();
+          })
+          .catch((err) => done(err));
+    });
+});
