88c978171f3967422725493df9de448f54d23328
MO <mohamedouazane@yahoo.com>
2024-06-06 14:53:47 +0000

    10-api/

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/available_payments', (req, res) => {
+    const obj = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      };
+    res.send(obj);
+});
+
+app.post('/login', (req, res) => {
+    if (req.body && req.body.userName) {
+        const username = req.body.userName;
+        res.send(`Welcome ${username}`);
+    }
+});
+
+app.get('/cart/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!isNaN(Number(id))) {
+        res.send(`Payment methods for cart ${id}`);
+    } else {
+        res.status(404).send('Not Found');
+    }
+});
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+exports.module = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,70 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('APP testing', function() {
+
+    it('Correct status code', function(done) {
+        request('http://localhost:7865', function(_error, response, _body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('Correct Body', function(done) {
+        request('http://localhost:7865', function(_error, _response, body) {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+
+    it('Correct status code', function(done) {
+        request('http://localhost:7865/cart/0', function(_error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 0');
+            done();
+        });
+    });
+
+    it('testing with wrong id hello', function(done) {
+        request('http://localhost:7865/cart/hello', function(_error, response, _body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('testing with wrong id aaaa', function(done) {
+        request('http://localhost:7865/cart/aaa', function(_error, response, _body) {
+            expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('Correct Body', function(done) {
+        request('http://localhost:7865/cart/12', function(_error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('Payment methods for cart 12');
+            done();
+        });
+    });
+
+    it('Correct Body 120', function(done) {
+            request('http://localhost:7865/cart/120', function(_error, _response, body) {
+                expect(body).to.equal('Payment methods for cart 120');
+                done();
+            });
+    });
+    it('Correct Body for available_payments', function(done) {
+        request('http://localhost:7865/available_payments', function(_error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+            done();
+        });
+    });
+    it('Correct Body for available_payments', function(done) {
+        request('http://localhost:7865/login', function(_error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal(`Welcome ${body.userName}`);
+            done();
+        });
+    });
+});
